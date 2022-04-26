3e90ca0827c2e10c2372c638f669adc0d3b46e99
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-04-25 21:40:38 -0300

    Add routes
    criado arquivo routes para gerenciar as rotas, ainda tem algum erro no uso do react-router-dom v6 que direciona mas não renderiza os conteúdos das páginas

--- dev-match/src/index.js
+++ dev-match/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './Pages/Main';
+import AppRoutes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <AppRoutes />
   </React.StrictMode>
 );
 
