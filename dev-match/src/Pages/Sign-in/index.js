b51fe76129afe2d5b7eff2fb63f8e2954853ae34
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-04-27 19:41:31 -0300

    Refactor className
    refatoração para adequação dos nomes das classes de estilo ao padrão BEM

--- dev-match/src/Pages/Sign-in/index.js
+++ dev-match/src/Pages/Sign-in/index.js
@@ -1,11 +1,29 @@
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 function SignIn() {
-    return (
-        <div className=" container-signIn text-center">
-            <h1>Sign In</h1>
+  return (
+    <div className='container__form'>
+      <form className='form__sign-in'>
+        <div className='text-center mb-lg'>
+          <h1>Entrar</h1>
+          <Link to="/sign-up">Cadastre-se</Link>
         </div>
-    );
+        <div>
+          <div>
+            <label>E-mail</label>
+            <input id='email' type='text' placeholder='Digite seu e-mail' />
+          </div>
+          <div>
+            <label>Senha</label>
+            <input id='password' type='text' placeholder='Digite sua senha' />
+          </div>
+          <button className='btn__dark-blue'></button>
+        </div>
+      </form>
+
+    </div>
+  );
 }
 
 export default SignIn;
