c2365db3c7c3458de25724e3dba73d3dc9d06487
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-04-27 21:10:14 -0300

    Update SignIn
    finalizando estilos básicos da tela de SignIn ainda com bugs no tamanho do formulário e outras funcionalidades a serem aplicadas.

--- dev-match/src/Pages/Sign-in/index.js
+++ dev-match/src/Pages/Sign-in/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function SignIn() {
   return (
     <div className='container__form'>
-      <form className='form__sign-in'>
+      <form className='form form__sign-in '>
         <div className='text-center mb-lg'>
           <h1>Entrar</h1>
           <Link to="/sign-up">Cadastre-se</Link>
